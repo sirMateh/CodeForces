38ab3014522f1405bbb01e85dfec7b26df89ef14
sirMateh <81980725+sirMateh@users.noreply.github.com>
2026-03-10 16:37:53 -0300

    Update B. Queue at the School.py

--- B. Queue at the School.py
+++ B. Queue at the School.py
@@ -1,15 +1,24 @@
 n,t = map(int, input().split(' '))
 queue, lista = str(input('')), list()
+
 for i in range(0,n):
     lista.append(queue[i])
+
 for i in range(0,t):
-    for x in range(0,n):
-        if x <= n-1 and lista[x] == 'B' and lista[x+1] == 'G':
-            lista[]
+    pos = list()
+    for x in range(0,n):    
+        if x+1 <= n-1  and lista[x] == 'B' and lista[x+1] == 'G' and pos.count(x) == 0:            
+            lista[x], lista[x+1] = lista[x+1], lista[x]
+            pos.append(x), pos.append(x+1)
+queue = ''
 
-print(lista)
+for i in lista:
+    queue += i
+
+print(queue)
 
 
-##terminar em casa
+
+
           
             
