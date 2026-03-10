6ada1a69e66d5ec27cb8013a6cc524115c5b03c2
sirMateh <81980725+sirMateh@users.noreply.github.com>
2026-03-10 12:19:30 -0300

    Create B. Queue at the School.py

---
+++ B. Queue at the School.py
@@ -0,0 +1,15 @@
+n,t = map(int, input().split(' '))
+queue, lista = str(input('')), list()
+for i in range(0,n):
+    lista.append(queue[i])
+for i in range(0,t):
+    for x in range(0,n):
+        if x <= n-1 and lista[x] == 'B' and lista[x+1] == 'G':
+            lista[]
+
+print(lista)
+
+
+##terminar em casa
+          
+            
